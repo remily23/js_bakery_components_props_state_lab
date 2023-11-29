2fb4b144069d1c77af43cb7f58ef615ff80e7d58
Emily Reid <emily.reid31@gmail.com>
2023-11-29 17:36:52 +0000

    added initial CakeContainer to be fixed

--- src/containers/CakeContainer.js
+++ src/containers/CakeContainer.js
@@ -1,1 +1,45 @@
 import{ useState } from 'react';
+
+const CakeContainer = () => {
+
+    const cakes = {
+        cakeName: "Victoria Sponge",
+        ingredients: [
+            "eggs",
+            "butter",
+            "sugar",
+            "self-raising flour",
+            "baking powder",
+            "milk"
+       ],
+       price: 5,
+       rating: 5
+ }
+
+    cakeName: "Tea Loaf",
+      ingredients: [
+        "eggs",
+        "oil",
+        "dried fruit",
+        "sugar",
+        "self-raising flour",
+        "strong tea",
+     ],
+     price: 2,
+     rating: 3
+}
+
+{
+    cakeName: "Carrot Cake",
+      ingredients: [
+       "carrots",
+         "walnuts",
+         "oil",
+         "cream cheese",
+         "flour",
+         "sugar",
+      ],
+      price: 8,
+      rating: 5
+} 
+
